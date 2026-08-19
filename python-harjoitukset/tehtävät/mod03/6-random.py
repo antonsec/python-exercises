7ee4439460d245dfaf9252b0a6c6f9ba29df6a2e
Pierre Anton <pierre.antonhihi@gmail.com>
2026-08-19 11:27:56 +0300

    forgot to do 6.

---
+++ python-harjoitukset/tehtävät/mod03/6-random.py
@@ -0,0 +1,11 @@
+import random
+for i in range(3):
+    code1 = random.randint(0, 9)
+    print (f"{code1}",end="")
+
+print("\n")
+
+for i in range(4):
+    code2 = random.randint(1, 6)
+    print (f"{code2}",end="")
+print ()
