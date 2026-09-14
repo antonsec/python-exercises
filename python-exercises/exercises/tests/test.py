2a038acf1f8b5755a1751d1356e35c100cb3bd6d
Pierre Anton <pierre.antonhihi@gmail.com>
2026-09-14 10:16:18 +0300

    made game better

--- python-exercises/exercises/tests/test.py
+++ python-exercises/exercises/tests/test.py
@@ -13,5 +13,50 @@
 
 # print (text)
 
-# Practise
 
+# Largest number
+
+# n1 = int(input("Enter 1st number: "))
+# n2 = int(input("Enter 2nd number: "))
+# n3 = int(input("Enter 3rd number: "))
+
+# if n1 >= n2 and n1 >= n3:
+#     print (f"Largest number: {n1}")
+# elif n2 >= n1 and n2 >= n3:
+#     print (f"Largest number: {n2}")
+# else:
+#     print (f"Largest number: {n3}")
+
+
+# Positive number loop with points
+
+# points = 0
+# while True:
+#     n = int(input("Enter number: "))
+#     if n == 0:
+#         break
+#     elif n > 0:
+#         points += 1
+# print (f"Positive numbers: {points}")
+
+# Cities with Lists and reverse print using for x in reversed(y)
+
+# cities = []
+# for i in range(5):
+#     city = input("Enter city: ")
+#     cities.append(city)
+
+# print ("")
+
+# for city in reversed(cities):
+#     print (city)
+
+
+class Koira:
+    def __init__(self, rotu, nimi, vuosi):
+        self.rotu = rotu
+        self.nimi = nimi
+        self.vuosi = vuosi
+k1 = Koira('Bichon', 'Bianco', 2006)
+
+print (f"{k1.nimi} on {k1.rotu}. Hän on synytnyt {k1.vuosi}.")
--- python-exercises/game-project/game.py
+++ python-exercises/game-project/game.py
@@ -1,41 +1,68 @@
-name = "Pierre" #str(input("Hello traveler.. What may your name be?\n"))
-print (f"Welcome {name}. Let's get you started")
-age = 13 #int(input(f"Oh.. well hello {name}, good to meet you! May I ask ye age?\n"))
+inventory = ["Dagger", "Torch"] 
+health = 100
 
-inventory = [] # using this as players inventory
-
-def adding_item(item):
-    inventory.append(item) #using this function to add items ti inventory to avoid repetition
-
-def showing_items(): # this function goes through every item in players inventory
-    for i in inventory:
-        print (i)
+class Players: # adding class Players and giving player name, age and inventory
+    def __init__(self, name, age, inventory, health):
+        self.name = name
+        self.age = age
+        self.inventory = inventory
+        self.health = health
 
 def profile(): # this function prints the players profile
     print (f"--- Player profile ---")
-    print (f"Name: {name}")
-    print (f"Age: {age}")
+    print (f"Name: {player.name}")
+    print (f"Age: {player.age}")
+    print (f"Health: {player.health}")
+    print ("")
     print (f"Inventory:")
     showing_items()
+
+def adding_item(item): #  this function to adds items to inventory to avoid repetition
+    player.inventory.append(item) 
+
+def showing_items(): # this function goes through every item in players inventory and prints them
+        if not player.inventory: # checking if inventory empty
+            print (f"Empty inventory..")
+            return
+        
+        for items in player.inventory: # printing inventory if it's not empty
+            print (items)
+
+
+name = input("Hello traveler.. What is your name? ")
+age = int(input(f"\nOh.. well hello {name}, good to meet you! May I ask ye age? "))
+player = Players(name, age, inventory, health) # adding player into class Players
+
+
     
 while True: # looping the program untill user input an empty string
-    if age < 12: # checking age
-        print ("Yikes! Sorry! You're too young for this game! Try again in a few years!")
+    if player.age < 12: # checking age
+        print ("Yikes! Sorry.. you're too young for this game! Try again in a few years!")
         break
 
     print ("\nOptions: 1 Add items to inventory, 2 Check inventory and 3 Prints player profile.\n") # printing menu
     command = input("Command: ") # asking user for a command
+
     if command == "": # if empty string end program
-        print (f"See you again {name}...")
+        print (f"See you again {player.name}...")
         break
 
     if command == "1": # first option
-        new_item = input(f"What item would you like to add? ") # asking user for item
-        adding_item(new_item) # addig item using adding_item function
-        print (f"Added {new_item} to inventory!") # printing what item added to user
+        new_item = " "
+        print ("Alright adding items!\n")
+        while new_item != "": # empty line ends adding item program
+            new_item = input(f"\nWhat item would you like to add? ")
+
+            if new_item == "":
+                print ("\nDone adding items...")
+                break
+             # asking user for item
+            adding_item(new_item) # addig item using adding_item function
+            print (f"Added {new_item} to inventory!") # printing what item added to user
+
 
     elif command =="2": #showing user inventory
-        print ("Your invetory is... interesting to say the least.\nHeres what you have:")
+        print ("\nYour invetory is... interesting to say the least.\nHeres what you have:")
         showing_items() # printing user inventory using showing_items function
 
     elif command == "3": 
